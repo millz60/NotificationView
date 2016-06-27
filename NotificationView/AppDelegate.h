87b1282a3f11fae8b378e9ba9603a9afdbedab97
millz60 <Matt@matts-macbook-pro.local>
2016-06-27 11:38:04 -0500

    Initial Commit

---
+++ NotificationView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NotificationView
+//
+//  Created by Matt Milner on 6/27/16.
+//  Copyright © 2016 Matt Milner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NotificationView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NotificationView
+//
+//  Created by Matt Milner on 6/27/16.
+//  Copyright © 2016 Matt Milner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
